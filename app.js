74f23f3788c43245e111c6945b8ad67b3769e6b2
LHUI <lihui96969@163.com>
2021-10-27 11:42:06 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, resp) {
+  const { format = "js", idx = 0, n = 1, mkt = "en-US", callback } = req.query;
+  const bingAPI = `http://cn.bing.com/HPImageArchive.aspx?format=${format}&idx=${idx}&n=${n}&mkt=${mkt}`;
+
+  http.get(bingAPI, function (res) {
+    const { statusCode } = res;
+    if (statusCode !== 200) {
+      resp.json({ error: "HTTP request failed" });
+    }
+
+    res.setEncoding("utf8");
+    let rawData = "";
+
+    res.on("data", (chunk) => (rawData += chunk));
+
+    res.on("end", () => {
+      if (format === "js") {
+        try {
+          const JSONData = JSON.parse(rawData);
+          if (callback) {
+            resp.jsonp(JSONData);
+          } else {
+            resp.json(JSONData);
+          }
+        } catch (e) {
+          resp.status(500).json({ error: "Parse failed" });
+        }
+      } else if (format === "xml" || format === "rss") {
+        resp.type("text/xml").send(rawData);
+      }
+    });
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`);
+});
